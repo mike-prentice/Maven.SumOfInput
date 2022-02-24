07be531c52e3296e3e06a7681883f9f437d7da4f
Mike <mike@Zipcoders-MacBook-Pro.local>
2022-02-24 13:09:08 -0500

    Working Version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter a number...");
 
+        Integer n = num.nextInt();
+        Integer sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
